8b93c3bb9bb5cb539b7de2b6c970d63882eb36a8
Erik Oliveira <erik.oliveira5779@gmail.com>
2023-04-27 16:36:45 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
