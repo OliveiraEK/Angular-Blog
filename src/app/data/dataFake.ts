fd3df41f2a4e3443d30f49c46f2846ad38396b23
Erik Oliveira <erik.oliveira5779@gmail.com>
2023-04-28 14:41:35 -0300

    Add rotas e Pagina dinâmica

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+{
+  "id":1,
+  "title": "A Jornada do Ninja: Uma Análise da Busca de Naruto pela Verdadeira Força.",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, id! Cumque, quis nostrum perferendis minus distinctio atque corrupti natus deserunt possimus, earum impedit. Repellat debitis consectetur ducimus assumenda voluptates iusto!",
+  "photoCover": "https://images8.alphacoders.com/487/thumbbig-487189.webp"
+},
+{
+  "id":2,
+  "title": "Turbulência em Konoha: Análise da Imagem do Time 7 em Meio à Tempestade em Naruto",
+  "description": "A cidade de Konoha estava em tumulto. Uma tempestade feroz atingiu a cidade, derrubando árvores e postes de luz, e transformando as ruas em rios lamacentos. Mas, em meio a toda essa confusão, uma imagem em particular chamou a atenção de todos. Era o Time 7, composto por Naruto Uzumaki, Sasuke Uchiha e Sakura Haruno, lutando juntos contra a tempestade. Os três estavam de pé em uma rua inundada, com água até os joelhos, segurando suas mochilas firmemente e olhando para a frente com determinação. Naruto era o primeiro a agir, utilizando suas habilidades de controle de chakra para criar um escudo de vento em torno deles, protegendo-os da chuva forte e do vento. Enquanto isso, Sasuke usava suas habilidades de fogo para iluminar a área, permitindo que eles vissem onde estavam pisando, e Sakura estava pronta para usar seus conhecimentos médicos caso alguém se machucasse. Os três trabalhavam em conjunto, saltando sobre os escombros e desviando de ramos quebrados. Eles estavam em uma missão importante, e não podiam deixar a tempestade os atrasar. Eles tinham que chegar ao outro lado da cidade para entregar um remédio vital para uma pessoa que precisava urgentemente. Mas a tempestade não era a única coisa que estava causando problemas. Alguns inimigos haviam aproveitado a situação para atacar a cidade, e o Time 7 teria que enfrentá-los também. Eles correram pelas ruas, saltando sobre poças e escombros, e logo se encontraram cercados por um grupo de ninjas inimigos. Naruto não hesitou. Ele criou clones de sombra e atacou os inimigos com uma série de socos e chutes rápidos. Sasuke, por sua vez, usou seu Sharingan para prever os movimentos dos inimigos e atacá-los com precisão mortal. E Sakura usou suas habilidades médicas para curar rapidamente os ferimentos de seus companheiros de equipe. A luta foi intensa, mas o Time 7 saiu vitorioso. Eles continuaram sua jornada, enfrentando vários outros obstáculos no caminho. Mas, no final, eles chegaram ao seu destino, entregando o remédio para a pessoa que precisava. A tempestade ainda estava furiosa lá fora, mas o Time 7 tinha realizado sua missão com sucesso. Eles estavam exaustos, mas também estavam felizes. Eles haviam enfrentado a tempestade juntos, lutando como uma equipe, e emergindo mais fortes do que nunca. Essa era a verdadeira essência do Time 7 - enfrentar desafios juntos, independentemente das circunstâncias.",
+  "photoCover": "https://images2.alphacoders.com/647/thumbbig-647514.webp"
+},
+{
+  "id":3,
+  "title": "Naruto e Kurama: A história de uma aliança improvável",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, id! Cumque, quis nostrum perferendis minus distinctio atque corrupti natus deserunt possimus, earum impedit. Repellat debitis consectetur ducimus assumenda voluptates iusto!",
+  "photoCover": "https://images7.alphacoders.com/303/thumbbig-303042.webp"
+},{
+  "id":4,
+  "title": "Obito Uchiha: A história de um jovem shinobi corrompido pela dor e pela vingança.",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, id! Cumque, quis nostrum perferendis minus distinctio atque corrupti natus deserunt possimus, earum impedit. Repellat debitis consectetur ducimus assumenda voluptates iusto!",
+  "photoCover": "https://images7.alphacoders.com/320/thumbbig-320350.webp"
+}
+];
